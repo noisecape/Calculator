2f803dff876b85fd10a3d1b739d56ee591c60f8f
Tommaso Capecchi <tommycaps@hotmail.it>
2019-03-28 23:58:55 +0100

    Working calculator
    Still need to catch various errors and implement the "Cancel" operator and catch the errors

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -5,9 +5,25 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
 public class Controller {
 
-    private Model calculatorBrain = new Model();
+    private Map<String, BiFunction<Integer,Integer,Integer>> operation = new HashMap<>();
+    private String expression;
+    private String accumulator;
+    private BiFunction<Integer,Integer,Integer> operator;
+
+    public Controller(){
+        this.operation.put("+",(x1,x2) -> x1 + x2);
+        this.operation.put("-", (x1,x2) -> x1 - x2);
+        this.operation.put("/", (x1,x2) -> x1 / x2);
+        this.operation.put("X", (x1,x2) -> x1 * x2);
+        this.expression = "";
+        this.accumulator = "";
+    }
 
     @FXML
     public Label resultLabel;
@@ -16,8 +32,20 @@ public class Controller {
     void pressedAnimation(MouseEvent event) {
         Button currentButton = (Button) event.getSource();
         pressedAnimation(currentButton);
-        calculatorBrain.setCalculatorDisplay(currentButton.getText());
-        resultLabel.setText(calculatorBrain.getCalculatorDisplay());
+        if (currentButton.getText().equals("=")){
+            resultLabel.setText(new Model(accumulator,expression,operator).evaluateExpression());
+            expression = "";
+            accumulator = "";
+            operator = null;
+        }else if(operation.containsKey(currentButton.getText())){
+            operator = operation.get(currentButton.getText());
+            accumulator = expression;
+            expression = "";
+            resultLabel.setText("0");
+        }else {
+            expression += currentButton.getText();
+            resultLabel.setText(expression);
+        }
     }
 
     @FXML
--- src/application/Model.java
+++ src/application/Model.java
@@ -2,75 +2,28 @@ package application;
 
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Model {
 
-    private ArrayList<String> expression;
-    private Map<String,Integer> numbers;
+    private String firstOperand;
+    private String secondOperand;
+    private BiFunction<Integer,Integer,Integer> operation;
 
-    public Model(){
-        expression = new ArrayList<>();
-        expression.add("0");
-        numbers = new HashMap<>(){{
-            put("0",0);
-            put("1",1);
-            put("2",2);
-            put("3",3);
-            put("4",4);
-            put("5",5);
-            put("6",6);
-            put("7",7);
-            put("8",8);
-            put("9",9);
-        }};
-
+    public Model(String firstOperand,String secondOperand, BiFunction<Integer,Integer,Integer> operation){
+        this.firstOperand = firstOperand;
+        this.secondOperand = secondOperand;
+        this.operation = operation;
+        this.evaluateExpression();
     }
 
-    public void setCalculatorDisplay(String digit){
-        expression.add(digit);
-        //if is zero, just add it once, the user can't add more than one zero as a first digit
-        //if is not a zero thus is a digit, add the digit
-        //if is an operand, check which operand it is and do the right operation
-        //display then the result on the screen
-    }
-
-    public String getCalculatorDisplay(){
-        Function<ArrayList<String>,String> printElementsToDisplay = (list) -> {
-            Iterator<String> listIterator = list.iterator();
-            String expression = "";
-            while(listIterator.hasNext()){
-                expression += listIterator.next();
-            }
-            return expression;
-        };
-        return printElementsToDisplay.apply(expression);
-    }
-
-    private void evaluate(){
-        System.out.println("EVALUATE EXPRESSION");
-    }
-
-    private int add(int a, int b){
-        return a+b;
-    }
-
-    private int sub(int a, int b){
-        return a-b;
-    }
-
-    private int mult(int a, int b){
-        return a*b;
-    }
-
-    private  int div(int a, int b){
-        if (b > 0)
-            return a/b;
-        else
-            System.out.println("ERROR: Cannot divide by 0");
-            return 0;
+    String evaluateExpression(){
+        Integer x1 = Integer.parseInt(firstOperand);
+        Integer x2 = Integer.parseInt(secondOperand);
+        return Integer.toString(operation.apply(x1,x2));
     }
 
 }
